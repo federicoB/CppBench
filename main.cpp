b0dd1d3181018c17997288e3d1015a9f024af9a2
federicoB <federicobertani96@gmail.com>
2016-06-08 17:19:30 +0200

    Added lambda, auto type and std::array test

--- main.cpp
+++ main.cpp
@@ -1,8 +1,19 @@
 #include <iostream>
+#include <array>
+#include <algorithm>
 
+//compile with c++14
 
+void lamdba() {
+    //initialize inline array with bracket initialization and auto keyword.
+    std::array<int,4> numberArray = {17,5,8,9};
+    std::for_each(numberArray.begin(),numberArray.end(),[](auto number){
+        std::cout << number << std::endl;
+    } );
+
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    lamdba();
     return 0;
 }
