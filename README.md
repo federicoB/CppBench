7c97d42be17ff3a8c7a56d661834cca76b7f3cbd
federicoB <federicobertani96@gmail.com>
2016-06-09 15:46:57 +0200

    Updated reame, added sort and smart pointer test.

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # CppBench
+Personal c++ testing repository.
+
+Here i tried:
+* lambda
+* automatic type deduction
+* std containers
+* Uniform Initialization Syntax
--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <array>
 #include <algorithm>
+#include <memory>
+#include <climits>
 
 //compile with c++14
 
@@ -10,10 +12,24 @@ void lamdba() {
     std::for_each(numberArray.begin(),numberArray.end(),[](auto number){
         std::cout << number << std::endl;
     } );
+    std::sort(numberArray.begin(), numberArray.end());
+    std::cout << "Now sorted" << std::endl;
+    for (auto element : numberArray) {
+        std::cout << element << std::endl;
+    }
 
 }
 
+void smartPointer() {
+    std::unique_ptr<std::vector<long int>> p(new std::vector<long int>(1280));
+    std::iota(p->begin(), p->end(), LONG_MIN);
+    for (auto element : *p) {
+        std::cout << element << std::endl;
+    }
+}
+
 int main() {
-    lamdba();
+    //lamdba();
+    smartPointer();
     return 0;
 }
